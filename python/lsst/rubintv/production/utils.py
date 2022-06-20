6015fd250b70d96e0ebe2f98bf3cf092cc503fbb
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2022-06-20 18:14:32 -0500

    Add utility functions for remaking plots

--- python/lsst/rubintv/production/utils.py
+++ python/lsst/rubintv/production/utils.py
@@ -21,6 +21,15 @@
 
 import logging
 
+from lsst.summit.utils.butlerUtils import getSeqNumsForDayObs, makeDefaultLatissButler
+from lsst.summit.utils.utils import dayObsIntToString, setupLogging
+from . import CHANNELS, PREFIXES
+from . import (ImExaminerChannel,
+               SpecExaminerChannel,
+               MountTorqueChannel,
+               MonitorChannel,
+               )
+
 __all__ = ["checkRubinTvExternalPackages",
            ]
 
@@ -72,3 +81,158 @@ def checkRubinTvExternalPackages(exitIfNotFound=True, logger=None):
 
     if exitIfNotFound and (not hasGoogleStorage or not hasEfdClient):
         exit()
+
+
+def getPlotSeqNumsForDayObs(channel, dayObs, bucket=None):
+    """Return the list of seqNums for which the plot exists in the bucket for
+    the specified channel.
+
+    Parameters
+    ----------
+    channel : `str`
+        The channel.
+    dayObs : `int`
+        The dayObs.
+    bucket : `google.cloud.storage.bucket.Bucket`, optional
+        The GCS bucket, created if not supplied.
+
+    Returns
+    -------
+    seqNums : `list` [`int`]
+        Sorted list of ints of the seqNums for which the specified plot exists.
+
+    Raises
+    ------
+    ValueError:
+        Raised if the channel is unknown.
+    """
+    if channel not in CHANNELS:
+        raise ValueError(f"Channel {channel} not in {CHANNELS}.")
+
+    if not bucket:
+        from google.cloud import storage
+        client = storage.Client()
+        bucket = client.get_bucket('rubintv_data')
+
+    dayObsStr = dayObsIntToString(dayObs)
+
+    prefix = f'{channel}/{PREFIXES[channel]}_dayObs_{dayObsStr}'
+    blobs = list(bucket.list_blobs(prefix=prefix))
+    existing = [int(b.name.split(f'{prefix}_seqNum_')[1].replace('.png', '')) for b in blobs]
+    return sorted(existing)
+
+
+def createChannelByName(channel, doRaise):
+    """Create a RubinTV Channel object using the name of the channel.
+
+    Parameters
+    ----------
+    channel : `str`
+        The name of the channel, as found in lsst.rubintv.production.CHANNELS.
+    doRaise : `bool`
+        Have the channel ``raise`` if errors are encountered while it runs.
+
+    Returns
+    -------
+    channel : `lsst.rubintv.production.<Channel>`
+        The lsst.rubintv.production Channel object.
+
+    Raises
+    ------
+    ValueError:
+        Raised if the channel is unknown, or creating by name is not supported
+        for the channel in question.
+    """
+    if channel not in CHANNELS:
+        raise ValueError(f"Channel {channel} not in {CHANNELS}.")
+
+    match channel:
+        case "summit_imexam":
+            return ImExaminerChannel(doRaise=doRaise)
+        case "summit_specexam":
+            return SpecExaminerChannel(doRaise=doRaise)
+        case "auxtel_mount_torques":
+            return MountTorqueChannel(doRaise=doRaise)
+        case "auxtel_monitor":
+            return MonitorChannel(doRaise=doRaise)
+        case "all_sky_current":
+            raise ValueError(f"{channel} is not a creatable by name.")
+        case "all_sky_movies":
+            raise ValueError(f"{channel} is not a creatable by name.")
+        case _:
+            raise ValueError(f"Unrecognized channel {channel}.")
+
+
+def remakePlotByDataId(channel, dataId):
+    """Remake the plot for the given channel for a single dataId.
+    Reproduces the plot regardless of whether it exists. Raises on error.
+
+    This method is very slow and inefficient for bulk processing, as it
+    creates a Channel object for each plot - do *not* use in loops, use
+    remakeDay() or write a custom scripts for bulk remaking.
+
+    Parameters
+    ----------
+    channel : `str`
+        The name of the channel.
+    dataId : `dict`
+        The dataId.
+    """
+    tvChannel = createChannelByName(channel, doRaise=True)
+    tvChannel.callback(dataId)
+
+
+def remakeDay(channel, dayObs, remakeExisting=False, notebook=True):
+    """Remake all the plots for a given day.
+
+    Parameters
+    ----------
+    channel : `str`
+        The name of the lsst.rubintv.production channel. The actual channel
+        object is created internally.
+    dayObs : `int`
+        The dayObs.
+    remakeExisting : `bool`, optional
+        Remake all plots, regardless of whether they already exist in the
+        bucket?
+    notebook : `bool`, optional
+        Is the code being run from within a notebook? Needed to correctly nest
+        asyncio event loops in notebook-type environments.
+
+    Raises
+    ------
+    ValueError:
+        Raised if the channel is unknown.
+    """
+    from google.cloud import storage
+
+    if channel not in CHANNELS:
+        raise ValueError(f"Channel {channel} not in {CHANNELS}")
+
+    if notebook:
+        # notebooks have their own eventloops, so this is necessary if the
+        # function is being run from within a notebook type environment
+        import nest_asyncio
+        nest_asyncio.apply()
+        setupLogging()
+
+    client = storage.Client()
+    bucket = client.get_bucket('rubintv_data')
+    butler = makeDefaultLatissButler()
+
+    allSeqNums = set(getSeqNumsForDayObs(butler, dayObs))
+    print(f"Found {len(allSeqNums)} seqNums to potentially create plots for.")
+    existing = set()
+    if not remakeExisting:
+        existing = set(getPlotSeqNumsForDayObs(channel, dayObs, bucket=bucket))
+        nToMake = len(allSeqNums) - len(existing)
+        print(f"Found {len(existing)} in the bucket which will be skipped, leaving {nToMake} to create.")
+
+    toMake = sorted(allSeqNums - existing)
+
+    # doRaise is False because during bulk plot remaking we expect many fails
+    # due to image types, short exposures, etc.
+    tvChannel = createChannelByName(channel, doRaise=False)
+    for seqNum in toMake:
+        dataId = {'day_obs': dayObs, 'seq_num': seqNum, 'detector': 0}
+        tvChannel.callback(dataId)
