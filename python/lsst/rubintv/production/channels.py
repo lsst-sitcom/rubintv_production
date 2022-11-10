bb2df774050ea4fa54764a102adbeb257a8b6382
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2022-11-10 14:49:04 -0800

    Add provisional new channels

--- python/lsst/rubintv/production/channels.py
+++ python/lsst/rubintv/production/channels.py
@@ -7,6 +7,11 @@ CHANNELS = ["summit_imexam",
             "auxtel_metadata",
             "auxtel_movies",
             "auxtel_isr_runner",
+            "startracker",
+            "startracker_analysis",
+            "startracker_wide",
+            "startracker_wide_analysis",
+            "startracker_metadata",
             ]
 
 PREFIXES = {chan: chan.replace('_', '-') for chan in CHANNELS}
