14d61347fa7c36b5130928335adaff2f3ab6ff86
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2023-02-20 17:33:37 -0800

    Fix path to runStarTrackerNightReport.py in temp script

--- scripts/summit/misc/runMisc.py
+++ scripts/summit/misc/runMisc.py
@@ -1,5 +1,5 @@
 import os
 dirName = os.path.dirname(__file__)
-fileToRun = os.path.join(dirName, '..', 'auxTel', 'runStarTrackerNightReport.py')
+fileToRun = os.path.join(dirName, '..', 'misc', 'runStarTrackerNightReport.py')
 print(f'Running {fileToRun}...')
 os.system(f'python {fileToRun}')
