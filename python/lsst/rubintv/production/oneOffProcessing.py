598f80bfd842b1958e4c32527a08dbcabfa1aca8
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2024-11-29 20:21:11 -0800

    Add mount metrics to table and add cell coloring

--- python/lsst/rubintv/production/oneOffProcessing.py
+++ python/lsst/rubintv/production/oneOffProcessing.py
@@ -469,6 +469,18 @@ class OneOffProcessor(BaseButlerChannel):
 
         writeMetadataShard(self.locationConfig.auxTelMetadataShardPath, dayObs, md)
 
+    @staticmethod
+    def _setFlag(
+        value: float, key: str, warningLevel: float, badLevel: float, outputDict: dict[str, Any]
+    ) -> dict[str, Any]:
+        if value >= warningLevel:
+            flag = f"_{key}"
+            outputDict[flag] = "warning"
+        elif value >= badLevel:
+            flag = f"_{key}"
+            outputDict[flag] = "bad"
+        return outputDict
+
     def runMountAnalysis(self, expRecord: DimensionRecord) -> None:
         errors, data = calculateMountErrors(expRecord, self.efdClient)
         if errors is None or data is None:
@@ -478,7 +490,43 @@ class OneOffProcessor(BaseButlerChannel):
         assert errors is not None
         assert data is not None
 
+        outputDict = {}
+
+        value = errors.imageImpactRms
+        key = "Mount motion image degradation"
+        outputDict[key] = f"{value:.3f}"
+        outputDict = self._setFlag(value, key, MOUNT_IMAGE_WARNING_LEVEL, MOUNT_IMAGE_BAD_LEVEL, outputDict)
+
+        value = errors.azRms
+        key = "Mount azimuth RMS"
+        outputDict[key] = f"{value:.3f}"
+
+        value = errors.elRms
+        key = "Mount elevation RMS"
+        outputDict[key] = f"{value:.3f}"
+
+        value = errors.rotRms
+        key = "Mount rotator RMS"
+        outputDict[key] = f"{value:.3f}"
+
+        value = errors.nReplacedAz
+        key = "Mount azimuth points replaced"
+        outputDict[key] = f"{value}"
+        outputDict = self._setFlag(value, key, N_REPLACED_WARNING_LEVEL, N_REPLACED_BAD_LEVEL, outputDict)
+
+        value = errors.nReplacedEl
+        key = "Mount elevation points replaced"
+        outputDict[key] = f"{value}"
+        outputDict = self._setFlag(value, key, N_REPLACED_WARNING_LEVEL, N_REPLACED_BAD_LEVEL, outputDict)
+
+        dayObs = expRecord.day_obs
+        seqNum = expRecord.seq_num
+        rowData = {seqNum: outputDict}
+        self.log.info(f"Writing mount analysis shard for {dayObs}-{seqNum}")
+        writeMetadataShard(self.shardsDirectory, dayObs, rowData)
+
         # TODO: DM-45437 Use a context manager here and everywhere
+        self.log.info(f"Creating mount plot for {dayObs}-{seqNum}")
         tempFilename = tempfile.mktemp(suffix=".png")
         plotMountErrors(data, errors, self.mountFigure)
         self.uploader.uploadPerSeqNumPlot(
@@ -491,23 +539,16 @@ class OneOffProcessor(BaseButlerChannel):
         self.mountFigure.clear()
         self.mountFigure.gca().clear()
 
-        imageImpact = errors.imageImpactRms
-        key = "Mount motion image degradation"
-        outputDict = {key: f"{imageImpact:.3f}"}
-        if imageImpact > MOUNT_IMAGE_WARNING_LEVEL:
-            flag = f"_{key}"
-            outputDict[flag] = "warning"
-        elif imageImpact > MOUNT_IMAGE_BAD_LEVEL:
-            flag = f"_{key}"
-            outputDict[flag] = "bad"
-
-        dayObs = expRecord.day_obs
-        seqNum = expRecord.seq_num
-        rowData = {seqNum: outputDict}
-        writeMetadataShard(self.shardsDirectory, dayObs, rowData)
+    def setFilterCellColor(self, expRecord: DimensionRecord) -> None:
+        filterName = expRecord.physical_filter
+        filterColor = getFilterColorName(filterName)
+        if filterColor:
+            md = {expRecord.seq_num: {"_Filter": filterColor}}
+            writeMetadataShard(self.shardsDirectory, expRecord.day_obs, md)
 
     def runExpRecord(self, expRecord: DimensionRecord) -> None:
         self.calcTimeSincePrevious(expRecord)
+        self.setFilterCellColor(expRecord)
         self.runMountAnalysis(expRecord)
 
     def callback(self, payload: Payload) -> None:
