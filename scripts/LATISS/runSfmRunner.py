48ca3fc08086fe9f92291fbbb5668e75613a23a4
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2025-03-11 17:32:43 -0700

    No, for real for real

--- scripts/LATISS/runSfmRunner.py
+++ scripts/LATISS/runSfmRunner.py
@@ -34,7 +34,7 @@ detectorDepth = workerNum
 
 locationConfig = getAutomaticLocationConfig()
 podDetails = PodDetails(
-    instrument=instrument, podFlavor=PodFlavor.SFM_WORKER, detectorNumber=detectorNum, depth=0
+    instrument=instrument, podFlavor=PodFlavor.SFM_WORKER, detectorNumber=detectorNum, depth=detectorDepth
 )
 print(
     f"Running {podDetails.instrument} {podDetails.podFlavor.name} at {locationConfig.location},"
