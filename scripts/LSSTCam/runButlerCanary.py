3185fa1baba95eef151668de3f868eed04dce2ef
Colin Slater <ctslater@uw.edu>
2025-04-21 11:04:18 -0700

    Canary logging fix?

--- scripts/LSSTCam/runButlerCanary.py
+++ scripts/LSSTCam/runButlerCanary.py
@@ -1,14 +1,36 @@
+# This file is part of rubintv_production.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import lsst.daf.butler as dafButler
 from lsst.daf.butler import DataCoordinate
-import logging
 import time
 import itertools
 import sys
+import logging
 
-log = logging.getLogger(__name__)
-log.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+                   format='%(asctime)s %(levelname)s %(message)s')
+log = logging.getLogger("butlerCanary")
+
+log.info("Starting butlerCanary")
 
 b = dafButler.Butler("embargo", collections="LSSTCam/defaults")
 
