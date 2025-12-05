02c416c95283b6a55653f07fa33ddc0180f0659b
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2025-12-05 11:46:09 -0800

    Change to use more modern data for testing

--- tests/ci/drip_feed_data.py
+++ tests/ci/drip_feed_data.py
@@ -4,8 +4,6 @@ import time
 t0 = time.time()
 
 from lsst.daf.butler import Butler, DimensionRecord  # noqa: E402
-from lsst.rubintv.production.payloads import Payload  # noqa: E402
-from lsst.rubintv.production.podDefinition import PodDetails, PodFlavor  # noqa: E402
 from lsst.rubintv.production.redisUtils import RedisHelper  # noqa: E402
 from lsst.rubintv.production.utils import getAutomaticLocationConfig  # noqa: E402
 
@@ -26,18 +24,18 @@ butler = Butler.from_config(
 
 redisHelper = RedisHelper(butler, locationConfig)
 
-# 86 - FAM CWFS image, goes as a FAM pair, but to the SFM pods
-# 87 - FAM CWFS image, goes as a FAM pair, but to the SFM pods
-# 88 - in focus, goes to SFM, expect a preliminary_visit_image mosaic etc.
+# 226 - in focus, goes to SFM, expect a preliminary_visit_image mosaic etc.
+# 227 - FAM CWFS image, goes as a FAM pair, but to the SFM pods
+# 228 - FAM CWFS image, goes as a FAM pair, but to the SFM pods
 # CWFS goes to AOS pods
-# 311 - a bias, to test cpVerify pipelines and mosaicing
+# 437 - a bias, to test cpVerify pipelines and mosaicing
 
 where = (
-    "exposure.day_obs=20250415 AND exposure.seq_num in (86..88,311)"
+    "exposure.day_obs=20251115 AND exposure.seq_num in (226..228,436)"
     f" AND instrument='{instrument}'"  # on sky!
 )
 records = list(butler.registry.queryDimensionRecords("exposure", where=where))
-assert len(records) == 4, f"Expected 1 record, got {len(records)}"
+assert len(records) == 4, f"Expected 4 records, got {len(records)}"
 
 for record in records:
     assert isinstance(record, DimensionRecord)
@@ -56,17 +54,7 @@ print(f"Butler init and query took {(time.time() - t0):.2f} seconds")
 time.sleep(2)  # make sure the head node has done the dispatch of the SFM image
 
 print("Pushing pair announcement signal to redis (simulating OCS signal)")
-redisHelper.redis.rpush("LSSTCam-FROM-OCS_DONUTPAIR", "2025041500086,2025041500087")
-
-# now send a single trigger manually to the guider pod
-where = f"exposure.day_obs=20250629 AND exposure.seq_num=340 AND instrument='{instrument}'"
-records = list(butler.registry.queryDimensionRecords("exposure", where=where))
-assert len(records) == 1, f"Expected a record for the guiders, got {len(records)}"
-podDetails = PodDetails(
-    instrument=instrument, podFlavor=PodFlavor.GUIDER_WORKER, detectorNumber=None, depth=0
-)
-payload = Payload([records[0].dataId], b"", "", who="")
-redisHelper.enqueuePayload(payload, podDetails)
+redisHelper.redis.rpush("LSSTCam-FROM-OCS_DONUTPAIR", "2025111500227,2025111500228")
 
 # do LATISS with the same drip-feeder
 instrument = "LATISS"
