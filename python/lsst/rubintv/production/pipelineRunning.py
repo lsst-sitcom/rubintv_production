c5ab91cb53b66c2937d8e3d5c6cdf78981654caa
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2025-12-18 06:26:27 -0800

    Fix unit tests to never see normal output run

--- python/lsst/rubintv/production/pipelineRunning.py
+++ python/lsst/rubintv/production/pipelineRunning.py
@@ -355,6 +355,7 @@ class SingleCorePipelineRunner(BaseButlerChannel):
             for d in self.butler.collections.defaults
             if d != self.locationConfig.getOutputChain(self.instrument)
         )
+        collections = newDefaults if not tip else [tip, *newDefaults]
         builder = TrivialQuantumGraphBuilder(
             pipeline_graph=pipelineGraph,
             butler=self.butler,
@@ -362,7 +363,7 @@ class SingleCorePipelineRunner(BaseButlerChannel):
             input_refs=inputRefs,
             dataset_id_modes=idGenerationModes,
             clobber=True,  # TBD by Jim as to whether this should be removed
-            input_collections=[tip] + newDefaults,
+            input_collections=collections,
             output_run=self.runCollection,
         )
         return builder, "", {}, butlerToReturn
@@ -416,13 +417,14 @@ class SingleCorePipelineRunner(BaseButlerChannel):
                 for d in self.butler.collections.defaults
                 if d != self.locationConfig.getOutputChain(self.instrument)
             )
+            collections = newDefaults if not tip else [tip, *newDefaults]
             builder = AllDimensionsQuantumGraphBuilder(
                 pipelineGraph,
                 self.butler,
                 where=where,
                 bind=bind,
                 clobber=True,
-                input_collections=[tip] + newDefaults,
+                input_collections=collections,
                 output_run=self.runCollection,
             )
             return builder, where, bind, self.cachingButler
@@ -452,12 +454,13 @@ class SingleCorePipelineRunner(BaseButlerChannel):
                     for d in self.butler.collections.defaults
                     if d != self.locationConfig.getOutputChain(self.instrument)
                 )
+                collections = newDefaults if not tip else [tip, *newDefaults]
                 builder = TrivialQuantumGraphBuilder(
                     pipeline_graph=pipelineGraph,
                     butler=self.butler,
                     data_ids=dataIds,
                     clobber=True,  # TBD by Jim as to whether this should be removed
-                    input_collections=[tip] + newDefaults,
+                    input_collections=collections,
                     output_run=self.runCollection,
                 )
                 return builder, "", {}, self.cachingButler
